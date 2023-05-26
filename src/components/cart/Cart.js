0f49357e016875de82e025a5574f1e6bc30a2ec0
tushari789 <tushar789imran@gmail.com>
2023-05-26 15:00:25 +0600

    order summary calculation added

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React from "react";
+import "./Cart.css";
 
-const Cart = ({cart}) => {
-    return (
-        <div>
-            <h4 className="cart-text">Older Summary</h4>
-        <p>Selected Items: {cart.length}</p>
-        </div>
-    );
+const Cart = (props) => {
+  const { cart } = props;
+//   console.log(cart);
+
+  let total = 0;
+  let shipping = 0;
+  let quentity = 0;
+  for (const product of cart) {
+    quentity = quentity + product.quentity;
+    total = total + product.price * product.quentity;
+    shipping = shipping + product.shipping;
+  }
+
+  const tax = parseFloat((total * 0.1).toFixed(2))
+
+  const grandTotal = total + shipping + tax
+
+  return (
+    <div className="cart">
+      <h4 className="cart-text">Older Summary</h4>
+      <p>Selected Items: {quentity}</p>
+      <p>Total Price: ${total}</p>
+      <p>Total Shipping Charge: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <p className="grand-total">Grand Total: ${grandTotal.toFixed(2)}</p>
+    </div>
+  );
 };
 
 export default Cart;
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -2,21 +2,78 @@ import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../product/Product";
 import Cart from "../cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] =useState([])
 
+  /*
+  ae process tai nice clean kore deoa holo.
+  useEffect(() => {
+    // console.log("product load before fetch");
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data)
+      // console.log('products loaded');
+      });
+  }, []);
+
+  useEffect(()=>{
+    // console.log('loacal storage first line', products);
+    const shoppingCart= getShoppingCart()
+    const savedCart =[];
+    for (const id in shoppingCart){
+      const addedProduct = products.find(product => product.id === id)
+      if(addedProduct){
+        const quentity = shoppingCart[id];
+        addedProduct.quentity = quentity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart)
+    // console.log('local storage finished');
+  },[products])
+  */
+
   useEffect(() => {
     fetch("products.json")
       .then((res) => res.json())
-      .then((data) => setProducts(data));
+      .then((data) =>setProducts(data));
   }, []);
 
-  const handleAddToCart = (product) => {
-    console.log(product);
-    const newCart =[...cart, product]
+
+  useEffect(()=>{
+    const shoppingCart = getShoppingCart();
+    const saveCart =[];
+    for(const id in shoppingCart){
+      const addedProduct = products.find(product => product.id === id);
+      if(addedProduct){
+        const quentity = shoppingCart[id];
+        addedProduct.quentity = quentity;
+        saveCart.push(addedProduct)
+      }
+    }
+    setCart(saveCart)
+  },[products])
+
+
+  const handleAddToCart = (selectedProduct) => {
+    console.log(selectedProduct);
+    let newCart = []; 
+    const exists = cart.find(product => product.id === selectedProduct.id);
+    if(!exists){
+      selectedProduct.quentity = 1;
+      newCart = [...cart, selectedProduct];
+    }else{
+      const rest = cart.filter(product => product.id !== selectedProduct.id);
+      exists.quentity = exists.quentity + 1;
+      newCart = [...rest, exists];
+    }
+    
     setCart(newCart)
+    addToDb(selectedProduct.id)
   };
 
   return (
